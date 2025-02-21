9bf8599ead1597ecbc285b2ecd7213dd77de9557
Chad Baldwin <cbaldwin@hchb.com>
2025-02-21 15:37:14 -0800

    Improve comments

--- src/Program.cs
+++ src/Program.cs
@@ -2,10 +2,11 @@
 
 // TODO: Exclude temp tables from all checks - There doesn't appear to be a programmatic way to identify temp tables other than "starts with #"
 // TODO: Reorganize methods into better classes instead of lumping everything together or making everything static
-// TODO: Change hardcoded file path to use input parameter
-// TODO: Expand to accpet text directly? Or stick with file paths? - Would support piping OBJECTDEFINITION direct from SQL
+// TODO: Expand to accept text directly? Or stick with file paths? - Would support piping OBJECTDEFINITION direct from SQL
 // TODO: Consider converting into compiled PowerShell cmdlet to support piping natively?
 // TODO: Test nested tables in update/delete statements - for example UPDATE x SET x.Col = 1 FROM (SELECT col FROM dbo.MyTable t WHERE t.ID < 100) x
+// TODO: Add unit testing - should be pretty easy to write 1 sql example for each oddball situation
+// TODO: Consider adding more detail to trigger relationships to show which transaction types are involved (INSERT, UPDATE, DELETE)
 
 class Program
 {
@@ -23,9 +24,12 @@ class Program
         using var reader = new StreamReader(filePath);
         var tree = (TSqlScript)parser.Parse(reader, out IList<ParseError> errors);
 
-        // Get top-level statements
+        // Split up into batches
         foreach (var batch in tree.Batches)
         {
+            // Process each top level statement in each batch
+            // Doing it this way so that we can maintain context of which object contains those statements
+            // Since the visitor pattern doesn't seem to play nice with maintaining context
             foreach (var topLevelStmt in batch.Statements)
             {
                 string? name;
@@ -41,6 +45,7 @@ class Program
                 }
                 else
                 {
+                    // for now - all other cases are thrown out as we're only interested in figuring out object level dependencies, not script level
                     continue;
                 }
 
@@ -54,6 +59,7 @@ class Program
 
 class StatementVisitor(string topLevelObject) : TSqlFragmentVisitor
 {
+    // Generic top level visitor just so we can add debugging messages when needed
     public override void Visit(TSqlStatement statement)
     {
         //Console.WriteLine($"  {statement.GetType().Name}");
@@ -132,25 +138,30 @@ class StatementVisitor(string topLevelObject) : TSqlFragmentVisitor
         WriteDotRelationship(topLevelObject, GetSchemaObjectNameString(statement.TableName), "TRUNC");
     }
 
+    // TODO: Need to test EXEC statements with scalar functions - not sure if ScriptDom can tell the difference
     public override void Visit(ExecuteStatement statement)
     {
+        // Standard stored procedure exec statement - EXEC dbo.usp_MyProc;
         if (statement.ExecuteSpecification.ExecutableEntity is ExecutableProcedureReference proc)
         {
             WriteDotRelationship(topLevelObject, GetSchemaObjectNameString(proc.ProcedureReference.ProcedureReference.Name), "EXEC");
         }
 
+        // EXEC with string list - EXEC ('SELECT * FROM dbo.MyTable');
         if (statement.ExecuteSpecification.ExecutableEntity is ExecutableStringList)
         {
             WriteDotRelationship(topLevelObject, "EXEC()", "EXEC");
         }
     }
 
+    // Seems useful to note relationships when messages are fired off to queues?
     public override void Visit(BeginDialogStatement statement)
     {
         WriteDotRelationship(topLevelObject, $"{statement.InitiatorServiceName.Value}.{statement.ContractName.Value}", "QUEUE");
     }
 
     // Helper class to visit all table references in the FROM clause
+    // This is necessary when UPDATE and DELETE statements use aliases for the target table, so we need to search the joins
     public class TableReferenceVisitor(string alias) : TSqlFragmentVisitor
     {
         public string? FoundObjectName { get; private set; }
